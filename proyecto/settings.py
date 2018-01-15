67bdc48aad97a3a56fdac4c14fb480ea6b6b3055
Alfredo Ch <alfre_096@hotmail.com>
2018-01-14 23:56:19 -0500

    login heroku 3

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+import dj_database_url
+db_from_env = dj_Database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 LOGIN_REDIRECT_URL = reverse_lazy('home:index')
