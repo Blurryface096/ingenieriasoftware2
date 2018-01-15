5dd6a18f451d63ba5ab2da51260823d0f09d4cc5
Alfredo Ch <alfre_096@hotmail.com>
2018-01-14 23:58:58 -0500

    login heroku 4

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 import dj_database_url
-db_from_env = dj_Database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
