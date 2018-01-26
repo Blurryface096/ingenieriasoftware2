02943983b3d18a6c1a8f94c0065063bf07a074b0
seem96ul <seem_96@outlook.com>
2018-01-25 23:48:50 -0500

    commit prueba html23

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from models import Preguntas
+from . import Preguntas
+
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
