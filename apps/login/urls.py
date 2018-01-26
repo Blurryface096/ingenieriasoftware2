77e36cf114a28b295746f78dd467a05c38b09e37
seem96ul <seem_96@outlook.com>
2018-01-25 23:33:17 -0500

    commit prueba html14

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from apps.login.views import index, ingresar
 
-app_name = 'login' 
+app_name = 'login'
 urlpatterns = [
-    path('', index),
+    path('login/', index),
     path('ingresar/', ingresar, name='ingreso'),
 ]
