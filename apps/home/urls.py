0b5a2e89003e7e8924d385db3468ff7e662fd799
seem96ul <seem_96@outlook.com>
2018-01-25 22:33:34 -0500

    commit prueba html12

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from apps.home.views import home
+from apps.home.views import trivia
 app_name = 'home'
 urlpatterns = [
     path('', home, name='index'),
