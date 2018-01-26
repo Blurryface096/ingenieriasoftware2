32d425043821e719371fe644125cdfc94641b420
seem96ul <seem_96@outlook.com>
2018-01-26 15:52:37 -0500

    urlfix

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from apps.home.views import home
 from apps.home.views import trivia
+from . import views
 app_name = 'home'
 urlpatterns = [
     path('', home, name='index'),
-    path('/trivia', trivia),
+    url(r'^trivia$',views.trivia)
 ]
