f1800c1fb382da5acf1abca00e2b0e3a59188ee8
seem96ul <seem_96@outlook.com>
2018-01-27 12:39:23 -0500

    gg

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'proyecto","static","static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'proyecto','static','static_root')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'proyecto","static","static_dirs'))
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'proyecto','static','static_dirs')
 LOGIN_REDIRECT_URL = reverse_lazy('home:index')
