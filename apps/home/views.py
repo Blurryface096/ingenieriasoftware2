1735117a1b4cc5b90a27fd93e09953eb272c1f1a
seem96ul <seem_96@outlook.com>
2018-01-28 13:50:32 -0500

    wachin

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,11 +8,11 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required(login_url='')
+@login_required(login_url='/login/')
 def home(request):
     return render(request, 'home/home.html')
 
-@login_required(login_url='')
+@login_required(login_url='/login/')
 def trivia(request):
     preguntas = Preguntas.objects.all()
     return render(request, 'home/trivia.html', { 'preguntas': preguntas})
