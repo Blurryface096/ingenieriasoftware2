74af6b1c35e81014e2aa534abab9126fc38e7ff8
seem96ul <seem_96@outlook.com>
2018-01-27 19:47:11 -0500

    validacion login v1

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from django.conf.urls import url
 from apps.home.views import home
 from apps.home.views import trivia
+from django.contrib.auth.decorators import login_required
 from . import views
 app_name = 'home'
 urlpatterns = [
-    path('', home, name='index'),
-    url(r'^trivia$',views.trivia)
+    path('', login_required(home), name='index'),
+    url(r'^trivia$',login_required(views.trivia))
 ]
