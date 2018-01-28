8f0e6f0ef147e5c58d368d5883ef6ce7e8d61dcb
seem96ul <seem_96@outlook.com>
2018-01-27 21:25:07 -0500

    validacion login v1

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from apps.home.models import Preguntas
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 @login_required(login_url='/')
@@ -17,3 +19,17 @@ def trivia(request):
 
 def logout_view(request):
     logout(request)
+
+
+def signup_view(request):
+
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:list')
+    else:
+
+
+        form=UserCreationForm()
+    return render(request,'accounts/signup.htlm',{'form':form})
