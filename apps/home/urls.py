15d71d019b5f948a29b42bf0f0e155c683383c16
seem96ul <seem_96@outlook.com>
2018-02-04 11:08:06 -0500

    fix polla

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     #url(r'^accounts/', include('apps.accounts.urls')),
     #url(r'^polla/$',polla,name='polla'),
     #url(r'^polla/<int:Juego.id>/$',polla,name='polla'),
-    path('polla/<Juego:Juego>/', polla, name='polla'),
+    path('polla/<int:Juego.id>/', polla, name='polla'),
     url(r'^equipo$',jugadores,name='equipo'),
     url(r'^crear_juego$',crear_juego,name='crear_juego'),
 
