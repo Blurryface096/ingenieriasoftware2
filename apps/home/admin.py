acb2a99ce0fc0b723a01a9381ec36c898a0888e2
seem96ul <seem_96@outlook.com>
2018-02-03 22:43:37 -0500

    integracion prueba 6

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -3,8 +3,10 @@ from .models import Preguntas
 from .models import Juego
 from .models import Partido
 from .models import ParticipacionPolla
+from .models import Equipo
 
 admin.site.register(Preguntas)
 admin.site.register(Juego)
 admin.site.register(Partido)
 admin.site.register(ParticipacionPolla)
+admin.site.register(Equipo)
