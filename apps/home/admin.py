5b1e693dcfaa05df35f8bd4ecc52ff82c01e0d2f
seem96ul <seem_96@outlook.com>
2018-02-03 22:39:51 -0500

    integracion prueba 6

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Preguntas
 from .models import Juego
+from .models import Partido
+from .models import ParticipacionPolla
 
 admin.site.register(Preguntas)
 admin.site.register(Juego)
