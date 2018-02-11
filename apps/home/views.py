c636c9178dd87f9978eabbce9ed7be4eea3d0fb3
seem96ul <seem_96@outlook.com>
2018-02-11 12:49:23 -0500

    balancemonetario fixed

--- apps/home/views.py
+++ apps/home/views.py
@@ -18,7 +18,7 @@ import datetime
 @login_required(login_url='')
 def home(request):
     nombre=request.user.username
-    balance=BalanceMonetario.objects.get(usuario=user.username).balance
+    balance=BalanceMonetario.objects.get(usuario=request.user.username).balance
     juego=Juego.objects.all()
     return render(request, 'home/home.html', { 'juego': juego, 'user':nombre,'balance':balance})
 
