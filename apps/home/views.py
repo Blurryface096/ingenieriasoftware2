1ce6e083f6e57913c0e328fe2502b90864ff0643
seem96ul <seem_96@outlook.com>
2018-02-18 18:29:15 -0500

    fix

--- apps/home/views.py
+++ apps/home/views.py
@@ -23,6 +23,7 @@ def home(request):
     nombre=request.user.username
 
     k=3
+    cadena="{% static 'img/c" &  str(k)  & ".png'%}"
     balance=BalanceMonetario.objects.get(usuario=request.user).balance
     juego=Juego.objects.filter(invitados=request.user)
     juego2=Juego.objects.filter(privacidad='Publico')
@@ -32,7 +33,7 @@ def home(request):
         balance=0
 
     listajuego=list(set(list(juego)+list(juego2)))
-    return render(request, 'home/home.html', { 'juego': listajuego, 'user':nombre,'balance':balance, 'k':k})
+    return render(request, 'home/home.html', { 'juego': listajuego, 'user':nombre,'balance':balance, 'cadena':cadena})
 
 def notificaciones(request):
     nombre=request.user.username
