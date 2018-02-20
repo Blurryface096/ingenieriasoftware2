6e9539aa6ea4e3e6f9017a6372cd7b8869082760
seem96ul <seem_96@outlook.com>
2018-02-19 22:19:50 -0500

    fix estilo

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -146,8 +146,9 @@ USE_I18N = True
 USE_L10N=False
 
 USE_TZ = True
-
-DECIMAL_SEPARATOR='Default'
+THOUSAND_SEPARATOR =','
+DECIMAL_SEPARATOR='.'
+USE_THOUSAND_SEPARATOR=True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
