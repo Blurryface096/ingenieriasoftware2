ffcc17d8245ab1cc997f7120a11343d70bf2f80f
seem96ul <seem_96@outlook.com>
2018-02-19 22:43:39 -0500

    imagenes futbol

--- apps/home/views.py
+++ apps/home/views.py
@@ -353,7 +353,7 @@ def modificar_balance(request):
 def trivia_juego(request,juego):
     juego=Juego.objects.get(id=juego)
     listapreg=Preguntas.objects.all().order_by('id')
-    
+    preguntas=[]
     if len(listapreg)>10:
         while len(preguntas)<10:
             for i in listapreg:
