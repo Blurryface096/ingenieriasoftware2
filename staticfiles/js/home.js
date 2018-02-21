020c9cd44bdf289301fd6edff20b0f6d4136a3af
seem96ul <seem_96@outlook.com>
2018-02-21 00:08:46 -0500

    enviar evento

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -19,7 +19,7 @@ if(browser == undefined){
 browser='Chrome'
 }
 function enviar_evento() {
-var accion=document.getElementById('GuardarEvento').tagName;
+var accion=document.getElementsByName('GuardarEvento').id;
 
   data={
   	'Tipo ': accion,
