64a4f27b781b3f4a05263c96304624179b264be9
seem96ul <seem_96@outlook.com>
2018-02-20 21:28:28 -0500

    ajax

---
+++ staticfiles/js/home.js
@@ -0,0 +1,25 @@
+BASE_URL="https://guachita-analytics.herokuapp.com/"
+alert("LEYO EL JS")
+
+
+$( "#crear_juego" ).click(function() {
+alert("DETECTÓ EL CLICK")
+  data={
+  	Tipo : “click”,
+  	URL_Actual : “www.guachita.com”,
+  	URL_Destino : “www.guachita.com/home”,
+  	Browser : “Mozilla”,
+  	Plataforma: “Win32”,
+  	Language: “en-US”,
+  }
+
+  $.ajax({
+    type: "POST",
+    url: BASE_URL + "sendEvent/",
+    data: "data=" + JSON.stringify(data),
+    async: false,
+    success: function (data) {
+      alert("TODO OK :P")
+      }
+  });
+});
