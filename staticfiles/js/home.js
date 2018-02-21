141abdb11c5e33d28168838119fa52b1b04e146e
seem96ul <seem_96@outlook.com>
2018-02-20 23:05:52 -0500

    enviar evento

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -2,7 +2,10 @@ BASE_URL="https://guachita-analytics.herokuapp.com/";
 var f = new Date();
 fecha=f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear();
 hora=f.getHours()+":"+f.getMinutes()+":"+f.getSeconds();
+var version=navigator.appVersion;
 
+var division=split(",");
+var browser=split[2];
 
 
 
@@ -13,7 +16,7 @@ function enviar_evento() {
     'Tipo ': 'Click',
   	'URL_Actual ': document.URL,
   	'URL_Destino ': document.referrer,
-    //'Browser': String(navigator.appVersion),
+    'Browser': browser
     'Modo Online':navigator.onLine,
   	'Plataforma': navigator.platform,
   	'Language': navigator.language,
