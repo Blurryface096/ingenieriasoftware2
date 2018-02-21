88b1a2fb00a131139c3e6fe570ee280baf2ccf26
Alfredo Chuquihuaranga <20140329@aloe.ulima.edu.pe>
2018-02-21 01:35:46 -0500

    fix reporte

--- staticfiles/js/analiticas.js
+++ staticfiles/js/analiticas.js
@@ -1,14 +1,12 @@
 BASE_URL="https://guachita-analytics.herokuapp.com/";
 
 var app = angular.module('analiticas', []);
-app.controller('reporte1', function($scope,$http) {
-
-
+app.controller('reporte1', function($scope, $http) {
   $http({
         method : "POST",
         url : BASE_URL + "getReport/",
     }).then(function mySuccess(response) {
-        console.log(response.data);
+        console.log(response.data.collection);
         $scope.eventos = response.data.collection;
     }, function myError(response) {
         alert("TODO NO OK :(");
