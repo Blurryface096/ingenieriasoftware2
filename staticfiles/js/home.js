d07f47878bac14b19ebfa990c3d8fc7477f902f3
seem96ul <seem_96@outlook.com>
2018-02-20 22:30:57 -0500

    ajax

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -1,19 +1,29 @@
 BASE_URL="https://guachita-analytics.herokuapp.com/";
+var f = new Date();
+fecha=f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear()
+hora=f.getHours()+":"+f.getMinutes()+":"+f.getSeconds();
 
+tiempoactual=hora +" "fecha
 
 
 function enviar_evento() {
-alert("DETECTÓ EL CLICK");
+accion= document.["GuardarEvento"].value;
   data={
-  	'Tipo ': 'click',
-  	'URL_Actual ': 'www.guachita.com',
-  	'URL_Destino ': 'www.guachita.com/home',
-  	'Browser' : 'Mozilla',
-  	'Plataforma': 'Win32',
-  	'Language': 'en-US',
+  	'Tipo ': accion,
+  	'URL_Actual ': document.referrer,
+  	'URL_Destino ': document.URL,
+    'Tipo Brower':navigator.appCodeName,
+    'Producto':navigator.product,
+    'Browser' : navigator.appName,
+    'Modo Online':navigator.onLine,
+  	'Plataforma': navigator.platform,
+  	'Language': navigator.language,
+    'Fecha':fecha,
+    'Hora':hora
   }
 
   $.ajax({
+    alert("entró al ajax");
     type: "POST",
     url: BASE_URL + "sendEvent/",
     data: "data=" + JSON.stringify(data),
