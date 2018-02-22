334b0d097ecb0563db058110c3f8e7d1f63b02c2
Alfredo Chuquihuaranga <20140329@aloe.ulima.edu.pe>
2018-02-21 20:46:10 -0500

    order by table analytics

--- staticfiles/js/analiticas.js
+++ staticfiles/js/analiticas.js
@@ -2,6 +2,15 @@ BASE_URL="https://guachita-analytics.herokuapp.com/";
 
 var app = angular.module('analiticas', []);
 app.controller('reporte1', function($scope,$http) {
+
+  var data = [{
+    values: [19, 26, 55],
+    labels: ['Residential', 'Non-Residential', 'Utility'],
+    type: 'pie'
+  }];
+
+  Plotly.newPlot('browsers', data);
+
   $http({
         method : "POST",
         url : BASE_URL + "getReport/",
