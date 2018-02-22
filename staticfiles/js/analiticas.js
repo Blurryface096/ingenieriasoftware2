84ab46d4e8158ce3dcfad1b08204f270711502b1
Alfredo Chuquihuaranga <20140329@aloe.ulima.edu.pe>
2018-02-21 21:06:12 -0500

    order by table analytics

--- staticfiles/js/analiticas.js
+++ staticfiles/js/analiticas.js
@@ -57,8 +57,15 @@ app.controller('reporte1', function($scope,$http) {
         type: 'pie'
       }];
 
+      var data3 = [{
+          x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+          y: [1, 3, 6],
+          type: 'scatter'
+        }
+      ];
       Plotly.newPlot('browsers', data);
       Plotly.newPlot('plataformas', data2);
+      Plotly.newPlot('visitas', data3);
 
 
     }, function myError(response) {
