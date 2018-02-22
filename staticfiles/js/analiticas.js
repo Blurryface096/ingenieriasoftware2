1daa2ca9298ab1cf0d2ef0bb792303c105ddd263
Alfredo Chuquihuaranga <20140329@aloe.ulima.edu.pe>
2018-02-21 20:57:16 -0500

    order by table analytics

--- staticfiles/js/analiticas.js
+++ staticfiles/js/analiticas.js
@@ -12,6 +12,11 @@ app.controller('reporte1', function($scope,$http) {
       chromes=0
       firefox=0
       safari=0
+
+      win=0
+      linux=0
+      mac=0
+
       for (var i = 0; i < response.data.collection.length; i++) {
         evento = $scope.eventos[i]
         if (evento.Browser=='Chrome') {
@@ -21,14 +26,34 @@ app.controller('reporte1', function($scope,$http) {
         } else {
           safari=safari+1
         }
+
+        if(evento.Plataforma=='Win32'){
+          win=win+1
+        } else if (evento.Plataforma=='Linux x86_64') {
+          linux=linux+1
+        } else {
+          mac=mac+1
+        }
       }
       total = chromes + firefox + safari
       chr = chromes/total
       fire = firefox/total
       safa = safari/total
+
+      total2=win+linux+mac
+      w=win/total2
+      l=linux/total2
+      m=mac/total2
+
       var data = [{
         values: [chr, fire, safa],
         labels: ['Chrome', 'Firefox', 'Otros'],
+        type: 'pie'
+      }];
+
+      var data2 = [{
+        values: [l, w, m],
+        labels: ['Linux', 'Windows', 'Otros'],
         type: 'pie'
       }];
 
