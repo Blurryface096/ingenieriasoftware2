039d1191735c7ef3245be5560a1931a51b7f21ad
Alfredo Chuquihuaranga <20140329@aloe.ulima.edu.pe>
2018-02-21 20:51:47 -0500

    order by table analytics

--- staticfiles/js/analiticas.js
+++ staticfiles/js/analiticas.js
@@ -9,31 +9,33 @@ app.controller('reporte1', function($scope,$http) {
     }).then(function mySuccess(response) {
       console.log(response.data)
       $scope.eventos = response.data.collection;
+      chromes=0
+      firefox=0
+      safari=0
+      for (var i = 0; i < response.data.collection.length; i++) {
+        evento = $scope.eventos[i]
+        if (evento.Browser=='Chrome') {
+          chromes=chromes+1
+        } else if (evento.Browser=='Firefox') {
+          firefox=firefox+1
+        } else {
+          safari=safari+1
+        }
+      }
+      total = chromes + firefox + safari
+      chr = chromes/total
+      fire = firefox/total
+      safa = safari/total
+      var data = [{
+        values: [chr, fire, safa],
+        labels: ['Residential', 'Non-Residential', 'Utility'],
+        type: 'pie'
+      }];
+
+      Plotly.newPlot('browsers', data);
+
+
     }, function myError(response) {
         alert("TODO NO OK :(");
     });
-    chromes=0
-    firefox=0
-    safari=0
-    for (var i = 0; i < $scope.eventos.length; i++) {
-      evento = $scope.eventos[i]
-      if (evento.Browser=='Chrome') {
-        chromes=chromes+1
-      } else if (evento.Browser=='Firefox') {
-        firefox=firefox+1
-      } else {
-        safari=safari+1
-      }
-    }
-    total = chromes + firefox + safari
-    chr = chromes/total
-    fire = firefox/total
-    safa = safari/total
-    var data = [{
-      values: [chr, fire, safa],
-      labels: ['Residential', 'Non-Residential', 'Utility'],
-      type: 'pie'
-    }];
-
-    Plotly.newPlot('browsers', data);
 });
